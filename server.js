cf2aecca017ab42204348ee476fbdc5bbca37fc7
Amritesh <djamritesh2002@gmail.com>
2024-10-18 16:00:15 +0530

    Set up /create-account route to validate fullName, email, and password, check for existing users, save new users, and return JWT access token on successful registration.

--- server.js
+++ server.js
@@ -34,9 +34,32 @@ app.post("/create-account",async(req,res)=>{
     return res.status(400).json({error:true,message:"Email is required"});
   }
   if(!password) {
-    return res.status(400).json({error:true,message:"Email is required"})
+    return res.status(400).json({error:true,message:"Password is required"})
   }
-})
+  const isUser = await User.findOne({email:email});
+  if(isUser) {
+    return res.json({
+      error:true,
+      message: "User already exist",
+    });
+  }
+  const user =new User({
+    fullName,
+    email,
+    password,
+  });
+  await user.save();
+  const accessToken =jwt.sign({user},process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:"30m",
+
+  });
+  return res.json({
+    error:false,
+    user,
+    accessToken,
+    message:"Registration Successful",
+  });
+});
 
 app.listen(8001, () => {
   console.log("Server is running on port 8001");
