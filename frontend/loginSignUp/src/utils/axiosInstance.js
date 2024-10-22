43b5e4b5c0a3255127211063cbe41ea695635c4f
Amritesh <djamritesh2002@gmail.com>
2024-10-22 14:53:42 +0530

    "Create axios instance with base URL and authorization token handling"

---
+++ frontend/loginSignUp/src/utils/axiosInstance.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { BASE_URL } from "./constants"; // Ensure this import is correct
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,  // Use the already imported BASE_URL
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
