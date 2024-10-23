07bb432dec661573c36dfef48ceaca7e5905ab82
Amritesh <djamritesh2002@gmail.com>
2024-10-23 12:39:10 +0530

    .

--- backend/utilities.js
+++ backend/utilities.js
@@ -2,14 +2,14 @@ const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1]; 
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return res.status(401).json({ error: true, message: "Access token required" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.status(403).json({ error: true, message: "Invalid token" });
     req.user = user;
-    next(); 
+    next();
   });
 }
 
