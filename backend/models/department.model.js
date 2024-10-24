02cee6d7645bda6e5c32b1d1fc77ec7f720841d4
Amritesh <djamritesh2002@gmail.com>
2024-10-24 15:55:22 +0530

    Created department schema with name, description, and createdOn fields.

---
+++ backend/models/department.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const departmentSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  description: String,
+  createdOn: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Department', departmentSchema);
